159c55a7440e2eb2305882566ea9f90875bccd94
Dizzymouse0 <npage1221@gmail.com>
2023-10-28 14:09:03 -0500

    html routes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,3 +8,20 @@ let notes = require('../../db/db.json');
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
+
+// POST request to make a note
+router.post('/notes', (req, res) => {
+    // creates a note in the db.json file
+    const newNote = req.body;  // matching call passed through from lib/notes
+    saveNote(newNote);
+    res.json(notes);
+});
+
+// delete request for API
+router.delete('/notes:id', (req, res) => {
+    //uses the ID to delete a note
+    notes = deleteNote(req.params.id);
+    res.json(notes);
+});
+
+module.exports = router;
