89b4cc620536ef26f2e21b2f2e0cbc2d8f98f502
dizzymouse0 <134219458+dizzymouse0@users.noreply.github.com>
2023-10-30 09:25:05 -0500

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,8 @@ const path = require('path');
 // HTML routes returning the notes.html files
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../note-taking-app/public/notes.html'));
+    //having issus with routing pages correctly
+    //errors with heroku deployment as well
 });
 
 // HTML routes returning the index.html files
